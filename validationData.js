927e07fc920746db3f5628a2ae2bff3dd81a6980
Sasha Prokofyev <ablehope@yandex.by>
2019-12-25 15:11:06 +0300

    added schems, routers, controllers, middlewares, logger

---
+++ validationData.js
@@ -0,0 +1,21 @@
+const { celebrate, Joi } = require('celebrate');
+
+const signupValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const signinValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  signupValidation,
+  signinValidation,
+};
