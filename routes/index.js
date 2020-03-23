d7ffb309b3e9180b96c1d3651c2c241944b35a8e
Sasha Prokofyev <ablehope@yandex.by>
2020-03-23 14:22:00 +0300

    added middleware cors

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const routes = require('express').Router();
 const BadRequestError = require('../errors/BadRequestError');
 const { resouceNotFound } = require('../consts');
 const auth = require('../middlewares/auth');
+const cors = require('../middlewares/cors');
 const routerArticles = require('./articles');
 const routerUsers = require('./users');
 const userAuthRoute = require('./userAuth');
 
-routes.use('/', userAuthRoute, auth, routerArticles, routerUsers);
+routes.use('/', userAuthRoute, auth, routerArticles, routerUsers, cors);
 routes.use('*', (res, req, next) => {
   next(new BadRequestError(resouceNotFound));
 });
